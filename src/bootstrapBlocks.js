31a59f22165ca22964a7ec367532433268295159
Roc <chengpeng19925@163.com>
2019-10-17 23:23:48 +0800

    完善内容

--- src/bootstrapBlocks.js
+++ src/bootstrapBlocks.js
@@ -7,19 +7,40 @@ export default function (editor, opt = {}) {
   const resizerBtm = { tl: 0, tc: 0, tr: 0, cl: 0, cr: 0, bl: 0, br: 0, minDim };
   const resizerRight = { ...resizerBtm, cr: 1, bc: 0, currentUnit, minDim, step };
 
+  const layout = '布局';
+  const getContainerName = fluid => { return fluid ? 'container-fulid' : 'container' };
+  const containerName = getContainerName(false);
+  const containerFluidName = getContainerName(true);
+
+  const containerAttr = {
+    class: containerName,
+    'data-gjs-droppable': `.row`,
+    'data-gjs-resizable': resizerBtm,
+    'data-gjs-name': containerName,
+    'style': 'min-height:100px;'
+  };
+
+  const containerFluidAttr = {
+    class: containerFluidName,
+    'data-gjs-droppable': `.row`,
+    'data-gjs-resizable': resizerBtm,
+    'data-gjs-name': containerFluidName,
+    'style': 'min-height:100px;'
+  };
+
   const rowAttr = {
-    class: 'container',
+    class: 'row',
     'data-gjs-droppable': `.col-md-1`,
     'data-gjs-resizable': resizerBtm,
-    'data-gjs-name': 'container',
+    'data-gjs-name': 'row',
     'style': 'min-height:100px;'
   };
 
   const colAttr = {
-    class: '.col-md-1',
-    'data-gjs-draggable': '.col-md-1',
+    class: 'col-md-1',
+    'data-gjs-draggable': '.row',
     'data-gjs-resizable': resizerRight,
-    'data-gjs-name': '.col-md-1',
+    'data-gjs-name': 'col-md-1',
     'style': 'min-height:100px;'
   };
 
@@ -40,23 +61,30 @@ export default function (editor, opt = {}) {
   const attrsCell = attrsToString(colAttr);
 
   //布局
-  bm.add('container', {
+  bm.add(containerName, {
     label: '容器',
-    category: 'container',
+    category: layout,
     //attributes: { class: 'gjs-fonts gjs-f-b1' },
-    content: `<div ${attrsRow}>这是容器</div>`
+    content: `<div ${attrsToString(containerAttr)}>这是容器</div>`
   });
 
-  bm.add('container-fluid', {
+  bm.add(containerFluidName, {
     label: '容器-fluid',
-    category: 'container',
+    category: layout,
     //attributes: { class: 'gjs-fonts gjs-f-b1' },
-    content: '<div class="container-fluid" style="min-height:100px;">这是容器</div>'
+    content: `<div ${attrsToString(containerFluidAttr)}>这是容器-fluid</div>`
   });
 
-  bm.add('column1', {
+  bm.add('row-1', {
+    label: '行',
+    category: layout,
+    //attributes: { class: 'gjs-fonts gjs-f-b1' },
+    content: `<div ${attrsRow}>这是行</div>`
+  });
+
+  bm.add('column-1', {
     label: '1列',
-    category: 'container',
+    category: layout,
     //attributes: { class: 'gjs-fonts gjs-f-b1' },
     content: `<div ${attrsCell}>这是列</div>`
   });
